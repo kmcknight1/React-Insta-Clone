65aa203b4a7e2c48a2358eaf77fcb2d5715bb406
kmcknight1 <katiemcknight1991@gmail.com>
2019-06-05 15:10:07 -0500

    PostsPage Component added and refactored code

--- insta-app/src/App.js
+++ insta-app/src/App.js
@@ -1,31 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import dummyData from "./dummy-data";
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
+import PostsPage from "./components/PostsPage/PostsPage";
 
 function App() {
-  let data = dummyData;
-
-  const [search, setSearch] = useState("");
-
-  if (search !== "") {
-    data = dummyData.filter(post => {
-      return post.username.includes(search.toLowerCase());
-    });
-  }
-
   return (
     <div className="app">
-      <header>
-        <SearchBar search={search} setSearch={setSearch} />
-      </header>
-
-      {data.length !== 0 ? (
-        data.map(item => <PostContainer data={item} />)
-      ) : (
-        <h2 className="no-results">No results found</h2>
-      )}
+      <PostsPage />
     </div>
   );
 }
